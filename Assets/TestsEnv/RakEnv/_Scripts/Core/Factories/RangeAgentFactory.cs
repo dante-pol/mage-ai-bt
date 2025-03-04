ca570180fb3f7644838f4ddaf4bea3c15786451e
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-04 14:52:05 +0300

    добавил фабрику ботов дальнего боя

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Core/Factories/RangeAgentFactory.cs
@@ -0,0 +1,25 @@
+﻿using Root.Core.Tools;
+
+namespace Root.Core.Entities.Agents.Range
+{
+    public class RangeAgentFactory : ABaseFactory
+    {
+        private const string PATH_TO_PREFAB = "";
+
+        private readonly RangeAgent _prefabAgent;
+
+        public RangeAgentFactory()
+        {
+            _prefabAgent = AssetsProvider.Load<RangeAgent>(PATH_TO_PREFAB);
+        }
+
+        public RangeAgent Create()
+        {
+            var agent = Instantiate(_prefabAgent);
+
+            agent.Construct();
+
+            return agent;
+        }
+    }
+}
