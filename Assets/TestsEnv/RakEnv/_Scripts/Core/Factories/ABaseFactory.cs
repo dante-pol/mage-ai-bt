b2476775e60756073184fe82c607cf9be5755709
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-04 14:22:29 +0300

    добавил базовою фабрику

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Core/Factories/ABaseFactory.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Root.Core.Entities.Agents.Range
+{
+    public abstract class ABaseFactory
+    {
+        protected T Instantiate<T>(T instance) where T : Object
+            => Object.Instantiate(instance);
+
+
+        protected T Instantiate<T>(T instance, Vector3 position, Quaternion orientation) where T : Object
+            => Object.Instantiate(instance, position, orientation);
+    }
+}
