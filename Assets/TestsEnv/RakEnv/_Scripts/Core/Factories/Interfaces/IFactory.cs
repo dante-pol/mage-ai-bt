8b6d7bcfeaed6dcbba31a7cbd67036c5985ef6f3
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-04 15:42:14 +0300

    save point

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Core/Factories/Interfaces/IFactory.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Root.Core.Factories
+{
+    public interface IFactory
+    {
+        T Create<T>() where T : Object;
+
+        T Create<T>(Vector3 position, Quaternion orientation) where T : Object;
+    }
+}
---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Tests/TestCommandCenter.cs
@@ -0,0 +1,23 @@
+﻿using Root.Core.Entities.Agents.Range;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Root.Tests
+{
+    public class TestCommandCenter : MonoBehaviour
+    {
+        public List<Agent> _melees;
+        public List<RangeAgent> _ranges;
+
+        private void Awake()
+        {
+            _ranges = FindObjectsOfType<RangeAgent>().ToList();
+
+            foreach (var range in _ranges)
+            {
+                range.Construct();
+            }
+        }
+    }
+}
