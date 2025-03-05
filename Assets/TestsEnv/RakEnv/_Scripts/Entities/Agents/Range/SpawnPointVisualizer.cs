af7002a3eb96d0be39d9293dee5713f97538626c
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-05 14:14:55 +0300

    добавлен скрипт визуализации точек спавна

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Agents/Range/SpawnPointVisualizer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Root.Core.Entities.Agents.Range
+{
+    public class SpawnPointVisualizer : MonoBehaviour
+    {
+        [SerializeField] Color _color;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = _color;
+
+            Gizmos.DrawSphere(transform.position, 1);
+        }
+    }
+}
