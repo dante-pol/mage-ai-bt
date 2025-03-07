5e637cf4be18c2c42d4a27f07f88298726d2583d
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-07 17:15:20 +0300

    добавил компонент для проигрываения звуков

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Agents/_Core/MeleeSounds.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Root
+{
+    public class MeleeSounds
+    {
+        private readonly AudioSource _agentAudioSource;
+
+        private readonly AudioSource _legsAudioSource;
+
+        private readonly IMeleeSoundConfig _config;
+
+        public MeleeSounds(IMeleeSoundConfig config, AudioSource agentSource, AudioSource legsSource)
+        {
+            _config = config;
+
+            _agentAudioSource = agentSource;
+
+            _legsAudioSource = legsSource;
+        }
+
+        public void PlayAttack()
+        {
+            _agentAudioSource.clip = _config.Attack;
+
+            _agentAudioSource.Play();
+        }
+
+        public void PlayDeath()
+        {
+            _agentAudioSource.clip = _config.Death;
+
+            _agentAudioSource.Play();
+        }
+
+        public void PlayTurnZombie()
+        {
+            _agentAudioSource.clip = _config.ZombieTransformation;
+
+            _agentAudioSource.Play();
+        }
+    }
+}
