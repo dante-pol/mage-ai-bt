a3db41613e87031e8598c111fcd029b52282b413
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-08 14:11:41 +0300

    добавил компонент для атаки бота милишника

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Agents/_Core/MeleeAttacker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Root
+{
+    public class MeleeAttacker
+    {
+        public bool IsFreeze { get; set; }
+
+        private readonly ITriggerHandler _triggerHandler;
+
+        private readonly MeleeAgent _agent;
+
+        private readonly IMeleeAgentConfig _config;
+
+        public MeleeAttacker(ITriggerHandler triggerHandler, MeleeAgent agent, IMeleeAgentConfig config)
+        {
+            _triggerHandler = triggerHandler;
+            _agent = agent;
+            _config = config;
+
+            _triggerHandler.TriggerEnterEvent += AttackHandler;
+        }
+
+        private void AttackHandler(Collider other)
+        {
+            if (IsFreeze) return;
+
+            IEntityAttacked entity = other.GetComponent<IEntityAttacked>();
+
+            if (entity == null) return;
+
+            entity.TakeAttack(new AttackProcess(_config.Damage));
+        }
+    }
+}
