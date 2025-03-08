a33a9afb3832aef47135d67c8e411627503a7285
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-08 14:11:06 +0300

    добавил шину для триггеров

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Agents/_Core/ChildTriggerHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Root
+{
+    public class ChildTriggerHandler : MonoBehaviour, ITriggerHandler
+    {
+        public event UnityAction<Collider> TriggerEnterEvent;
+
+        private void OnTriggerEnter(Collider other)
+            => TriggerEnterEvent?.Invoke(other);
+    }
+}
---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Agents/_Core/ITriggerHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Root
+{
+    public interface ITriggerHandler
+    {
+        event UnityAction<Collider> TriggerEnterEvent;
+    }
+}
