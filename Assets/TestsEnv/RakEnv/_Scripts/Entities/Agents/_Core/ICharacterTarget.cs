fd6bbbadfcd92033c1efb1290303c4c27ecfdd4f
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-11 15:18:23 +0300

    добавил интерфейс для общения агентов и игрока

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Agents/_Core/ICharacterTarget.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Root
+{
+    public interface ICharacterTarget
+    {
+        public Vector3 Position { get; }
+     
+        public int HeatPoints { get; }
+
+        public bool IsActiveUlt { get; }
+
+    }
+}
