6ea4f2687c504e987fcdfce1db33aeda47587382
Dmitry Rak <dim.drvlg@gmail.com>
2025-03-14 16:57:53 +0300

    добавил эффект взрыва

---
+++ Assets/TestsEnv/RakEnv/_Scripts/Entities/Agents/Range/ExplosiveMechanism.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Root.Core.Entities.Agents.Range
+{
+    public class ExplosiveMechanism
+    {
+        private readonly ParticleSystem _system;
+
+        public ExplosiveMechanism(ParticleSystem system)
+        {
+            _system = system;
+
+            var main = _system.main;
+
+            main.loop = false;
+            main.playOnAwake = false;
+
+            _system.gameObject.SetActive(true);
+        }
+
+        public void Play() 
+            => _system.Play();
+    }
+}
